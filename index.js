6becec3a791680fc3e2c98da86b51b84b9d1723b
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-10-31 14:55:23 +0000

    ex1 easy level

---
+++ index.js
@@ -0,0 +1,12 @@
+// Operators Branching and Loops 
+
+// Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+
+var num1=5;
+var num2=13;
+var num3=5;
+var num4=21;
+var num5=48;
+
+var sum = num1+ num2+ num3+ num4+ num5;
+console.log(sum);
