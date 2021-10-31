617fdd81e7260c620c264cc8c9b86cbd7fbd2ed4
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-10-31 15:03:23 +0000

    ex02 easy level

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ var num5=48;
 
 var sum = num1+ num2+ num3+ num4+ num5;
 console.log(sum);
+
+// Write a program to take a number input from user and determine whether the number is odd or even.
+
+var rs= require('readline-sync');
+var num =rs.question("Enter a number:");
+
+if(num%2===0){
+	console.log(num+" is a even number");
+}
+else {
+	console.log(num+" is a odd number");
+}
