4b836fd4797acaa7c5c9d26d2941c8f18cdabd6d
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-10-31 15:11:21 +0000

    ex03 easy

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 // Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
 
-var num1=5;
+/*var num1=5;
 var num2=13;
 var num3=5;
 var num4=21;
 var num5=48;
 
 var sum = num1+ num2+ num3+ num4+ num5;
-console.log(sum);
+console.log(sum);*/
 
 // Write a program to take a number input from user and determine whether the number is odd or even.
 
-var rs= require('readline-sync');
+/*var rs= require('readline-sync');
 var num =rs.question("Enter a number:");
 
 if(num%2===0){
@@ -21,4 +21,15 @@ if(num%2===0){
 }
 else {
 	console.log(num+" is a odd number");
-}
+}*/
+
+//Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
+
+var num1=129;
+var num2=251;
+
+var maxi= Math.max(num1,num2);
+var mini= Math.min(num1,num2);
+
+console.log("Minimum number is "+mini);
+console.log("Maximum number is "+maxi);
