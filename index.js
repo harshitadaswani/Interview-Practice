b3029c0ddb6ed9b5deffc3b830ab8512f5a68aaa
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-11-01 18:23:59 +0000

    ex01 medium

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ else {
 
 // Write program to take a month as an input from the user and find out whether the month has 31 days or not.
 
-var rs = require('readline-sync');
+/*var rs = require('readline-sync');
 var month= rs.question("Enter a month number:");
 
 if(month==='1'||month==='3'||month==='5'||month==='7'||month==='8'||month==='10'||month==='12') {
@@ -96,4 +96,22 @@ else if(month>12) {
 }
 else {
 	console.log("month has 30 days");
-}
+}*/
+
+// Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz". Your output should look something like this 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17 .....
+
+/*var i;
+for(i=1;i<=100;i++)	{
+	if(i%3===0 && i%5===0)	{
+		console.log("FizzBuzz");
+	}
+	else if(i%3===0)	{
+		console.log("Fizz")
+	}
+	else if(i%5===0)	{
+		console.log("Buzz")
+	}
+	else	{
+		console.log(i);
+	}
+}*/
