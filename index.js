4d9019d43b1d0398f8c381c97b8e4a4d336709d1
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-11-04 16:42:53 +0000

    minimum using rest parameter

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function power(base,exp)	{
 /*Example:
 Input: areaOfHexagon(10) ––> Output: 259.80*/
 
-var len;
+/*var len;
 var rs=require("readline-sync");
 
 len = rs.question("Enter length of hexagon:");
@@ -34,4 +34,49 @@ area(len);
 function area(side)	{
 	var result=3/2*1.732050807568877*side*side;
 	console.log(`${result.toFixed(2)}`); 
+}*/
+
+// Given a sentence, your functions should return the number of words in the sentence.
+/*Example:
+Input: noOfWords(We are neoGrammers) ––> Output: 3*/
+
+/*var sent;
+var rs= require('readline-sync');
+
+sent = rs.question("Enter a sentence: ");
+
+function wordCounter(text) {
+  var text = sent.value;
+  var wordCount = 0;
+  for (var i = 0; i <= text.length; i++) {
+    if (text.charAt(i) == ' ') {
+      wordCount++;
+    }
+  }
+	console.log(wordCount);
+}*/
+
+// Given n numbers, your function should return the minimum of them all. The number of parameters won't be accepted from user.
+/*Example:
+Input: findMin(3,5) ––> Output: 3
+Input: findMin(3,5,9,1) ––> Output: 1
+(Hint: Lookup rest parameters in JavaScript)*/
+
+/*syntax: function f(a, b, ...theArgs) {
+  // ...
 }
+
+function functionname(...parameters)   //... is the rest parameter (triple dots)
+{
+statement;
+}*/
+
+function mini(...theArgs) {
+  return theArgs.reduce((previous, current) => {
+    return Math.min(previous,current);
+  });
+}
+
+console.log(mini(1,2,3,4,5,6));
+console.log(mini(3,5));
+console.log(mini(3,5,9,1));
